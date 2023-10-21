f90d2df0b4596ff8a113bad6a4440179b94a751e
Jorge Vicuña <jvicuna2001@gmail.com>
2023-10-21 18:14:06 -0300

    Registrar modelos Entidad y Comunicado

--- cert2/comunicados/admin.py
+++ cert2/comunicados/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from .models import AdminEntidad
+from .models import AdminEntidad, Comunicado, Entidad
 
 # Register your models here.
+admin.site.register(Entidad)
+admin.site.register(Comunicado)
+
 class AdminEntidadInline(admin.StackedInline):
     model = AdminEntidad
     can_delete = False
