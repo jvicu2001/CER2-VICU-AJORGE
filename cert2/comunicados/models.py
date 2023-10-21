75d8c00786d3e2ce3c753bd817a0152187b696a0
Jorge Vicuña <jvicuna2001@gmail.com>
2023-10-21 18:13:23 -0300

    Asociar usuarios a entidades

--- cert2/comunicados/models.py
+++ cert2/comunicados/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
-class Entity(models.Model):
+class Entidad(models.Model):
     id = models.BigAutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
     logo = models.ImageField(upload_to='logos', null=True, blank=True)
@@ -22,7 +23,7 @@ class Comunicado(models.Model):
     detalle = models.CharField(max_length=1000)
     detalle_corto = models.CharField(max_length=100)
     tipo = models.CharField(max_length=1, choices=TIPO_CHOICES)
-    entidad = models.ForeignKey(Entity, on_delete=models.CASCADE)
+    entidad = models.ForeignKey(Entidad, on_delete=models.CASCADE)
     visible = models.BooleanField(default=True)
     fecha_publicacion = models.DateTimeField(auto_now_add=True) # Darle la fecha actual, ya que se crea cuando se publica
     fecha_expiracion = models.DateTimeField(null=True, blank=True)
@@ -33,3 +34,11 @@ class Comunicado(models.Model):
 
     def __str__(self):
         return self.titulo
+    
+# Modelo extension usuario, para relacionarlo con modelo Entidad
+class AdminEntidad(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    entidad = models.ForeignKey(Entidad, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
