c597cc34ec4c27ffcdb252dcf5f9a7ceabe94a87
Jorge Vicuña <jvicuna2001@gmail.com>
2023-10-22 16:36:52 -0300

    Prueba datos en template

--- cert2/comunicados/views.py
+++ cert2/comunicados/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Comunicado
 
 # Create your views here.
+def index(request):
+    comunicados = Comunicado.objects.all()
+
+    return render(
+        request, 
+        'comunicados/index.html',
+        context={
+            'comunicados': comunicados,
+        },)
