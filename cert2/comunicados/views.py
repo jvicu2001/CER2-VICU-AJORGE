2689839ac703f98f746ac291614f5c04ba560825
Jorge Vicuña <jvicuna2001@gmail.com>
2023-10-22 18:20:28 -0300

    Mostrar comunicados

--- cert2/comunicados/views.py
+++ cert2/comunicados/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from .models import Comunicado
+from .models import Comunicado, Entidad
 
 # Create your views here.
 def index(request):
     comunicados = Comunicado.objects.all()
+    entidades = Entidad.objects.all()
 
     return render(
         request, 
         'comunicados/index.html',
         context={
             'comunicados': comunicados,
+            'entidades': entidades
         },)
